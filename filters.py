d4dbe3153cf5467131764abdb3b091e2464a02f2
popomore <sakura9515@gmail.com>
2012-10-31 23:32:49 +0800

    add examples

--- filters.py
+++
@@ -1,8 +0,0 @@
-def reveal(content):
-    result = ''
-    arr = content.split('<hr/>')
-    for c in arr:
-        result += '<div class="slides"><section>'
-        result += c
-        result += '</section></div>'
-    return result
